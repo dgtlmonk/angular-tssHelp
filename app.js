e345333e523f8336df074a02fbae37c0dc06d0fe
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-05-24 05:51:00 +0800

    improved folder structure

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var routes = require('./routes');
 var path = require('path');
+var Mincer = require('mincer');
+
+var assets_env = new Mincer.Environment();
+    assets_env.appendPath('assets/app');
+    assets_env.appendPath('assets/css');
+    assets_env.appendPath('assets/js');
+
+var connect_assets = require('connect-assets');
+var app_assets = require('connect-assets');
+    app_assets.paths = ["app/"];
 
 
 var app = express();
@@ -11,11 +21,16 @@ app.use(express.static(__dirname + '/public')); // static files location
 app.use(express.static(__dirname + '/src')); // static files location
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(require('connect-assets')());
+app.use(connect_assets());
+app.use(app_assets());
+// app.use('/assets',Mincer.createServer(assets_env));
 
 // var router = express.Router();
 
-app.get('/', routes.index);
+// app.get('/', routes.index);
+app.get('/', function (req, res){
+ res.render ('index',{ title: 'TSS HELP CENTER'});
+});
 
 app.listen(port);
 console.log(' ----- Started app at port ' + port);
